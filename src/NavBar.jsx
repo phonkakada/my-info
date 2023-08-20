5b144cdc4ab6a87bf744ee0561bc36ef2721846a
PHON KAKADA <phonkakada@icloud.com>
2023-08-20 23:27:40 +0700

    first-commit

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -20,15 +20,15 @@ const NavBar = () =>{
       </div>
       <ListMenu />
       </>
-    )
+    ) 
   }
 
   const ListMenu = () => {
-    return (
+    return ( 
       <>
       <div className="container">
       <center>
-        <div id="mylist">
+        <div id="mylist"> 
           <div id="list-content">
           <p><Link to="/" className="list-item">About Me</Link></p>
           <p><Link to="/resume" className="list-item">Resume</Link></p>
@@ -40,32 +40,37 @@ const NavBar = () =>{
       </div>
       </>
     )
-  }
+  };
 
   const listmenu = document.getElementById("buttonNavBar");
   const list_items = document.getElementById("list-content")
   if (listmenu){ 
-    listmenu.addEventListener('mouseover' , ()=>{
+    listmenu.addEventListener('mouseover' , ()=>{ 
+      document.getElementById("mylist").style.display='block'
       document.getElementById("mylist").style.width = "100%" 
       document.getElementById("mylist").style.marginRight = "-20%" 
     })
     list_items.addEventListener("mouseover", () => {
+      document.getElementById("mylist").style.display='block' 
       document.getElementById("mylist").style.width = "100%" 
       document.getElementById("mylist").style.marginRight = "-20%" 
     })
     listmenu.addEventListener('mouseout' , ()=>{
+      setTimeout(      document.getElementById("mylist").style.display='none' , 1000)   
       document.getElementById("mylist").style.width = "0%" 
     })
     list_items.addEventListener('mouseout' ,() => {
       document.getElementById("mylist").style.width = "0%" 
-
+ 
     })
     listmenu.addEventListener('click' , ()=>{
       if (      document.getElementById("mylist").style.width == "0%"){
+      document.getElementById("mylist").style.display='block'
         document.getElementById("mylist").style.width = "100%" 
         document.getElementById("mylist").style.marginRight = "-20%" 
       }else{
         document.getElementById("mylist").style.width = "0%";
+      document.getElementById("mylist").style.display='none'
       }
     })
   }
